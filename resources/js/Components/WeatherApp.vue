40d603b990d6a42ef0541c9c127a216f48c17a21
Vasyl Hryn <murphy31337@gmail.com>
2024-10-15 14:56:48 +0300

    Design improvements

--- resources/js/Components/WeatherApp.vue
+++ resources/js/Components/WeatherApp.vue
@@ -3,12 +3,12 @@
         <div class="places-input text-gray-800">
             <input type="text" class="w-full">
         </div>
-        <div class="weather-container font-sans w-128 max-w-lg overflow-hidden bg-gray-900 shadow-lg mt-4">
+        <div class="weather-container font-sans w-128 max-w-lg rounded-3xl overflow-hidden bg-gray-900 shadow-lg mt-4">
         <div class="current-weather flex items-center justify-between px-6 py-8">
             <div class="flex items-center">
                 <div>
-                    <div class="text-6xl font-semibold">8</div>
-                    <div class="">Feels like</div>
+                    <div class="text-6xl font-semibold">8°C</div>
+                    <div class="mt-4">Feels like 8°C</div>
                 </div>
                 <div class="mx-5">
                     <div class="font-semibold">Cloudy</div>
@@ -17,6 +17,46 @@
             </div>
             <div>Two</div>
         </div> <!-- end current weather -->
+
+            <div class="future-weather text-sm bg-gray-800 px-6 py-8 overflow-hidden">
+            <div class="flex items-center">
+                <div class="w-1/6 text-lg text-gray-200">TUE</div>
+                <div class="w-4/6 px-4 flex items-center">
+                    <div>icon</div>
+                    <div class="ml-3">Cloudy with a chance of showers</div>
+                </div>
+              <div class="w-1/6 text-right">
+                  <div>5</div>
+                  <div>3</div>
+              </div>
+            </div>
+                <div class="flex items-center mt-8">
+                <div class="w-1/6 text-lg text-gray-200">TUE</div>
+                <div class="w-4/6 px-4 flex items-center">
+                    <div>icon</div>
+                    <div class="ml-3">Cloudy with a chance of showers</div>
+                </div>
+              <div class="w-1/6 text-right">
+                  <div>5</div>
+                  <div>3</div>
+              </div>
+            </div>
+                <div class="flex items-center mt-8">
+                <div class="w-1/6 text-lg text-gray-200">TUE</div>
+                <div class="w-4/6 px-4 flex items-center">
+                    <div>icon</div>
+                    <div class="ml-3">Cloudy with a chance of showers</div>
+                </div>
+              <div class="w-1/6 text-right">
+                  <div>5</div>
+                  <div>3</div>
+              </div>
+            </div>
+
+            </div> <!-- end future weather -->
+
+
+
         </div> <!-- end weather container -->
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ export default {
                 '128': '32rem'
             }
         },
+
     },
 
     plugins: [forms],
